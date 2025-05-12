8c5514f22a1df066cd4d1a6c9517f15a44c82885
sepehrnik7gmailcom <sepehrnik7@gmail.com>
2025-05-11 23:39:46 -0700

    Simulator can now use core.

---
+++ src/core/src/main/java/frc/robot/RobotCore.java
@@ -0,0 +1,6 @@
+package frc.robot;
+public class RobotCore {
+    public String getInfo() {
+        return "Core Library Functionality";
+    }
+}
--- src/simulator/src/main/java/frc/robotSimulator/Main.java
+++ src/simulator/src/main/java/frc/robotSimulator/Main.java
@@ -1,7 +1,10 @@
 package frc.robotSimulator;
 
+import frc.robot.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Robot Simulator!");
+        var a = new RobotCore();
+        System.out.println(a.getInfo());
     }
 }
